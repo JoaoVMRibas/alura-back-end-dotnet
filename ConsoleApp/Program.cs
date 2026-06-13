5cdce3e1a397a9865e8f158dcd65715434470e8d
João Ribas <joaovitormribas@gmail.com>
2026-06-13 18:52:08 -0300

    Menu adjustments

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,34 +1,5 @@
 ﻿using ConsoleApp.Models;
 
-//Artist artist = new Artist("Michael Jackson");
-
-//var music1 = new Music("Billie Jean", artist, 294, false);
-//var music2 = new Music("Beat It", artist, 258, true);
-//var music3 = new Music("Wanna Be Startin’ Somethin’", artist, 363, true);
-
-//var album1 = new Album("Thriller",artist);
-//album1.Musics.Add(music1);
-//album1.Musics.Add(music2);
-//album1.Musics.Add(music3);
-
-//music1.DisplayInformation();
-//artist.DisplayAlbumsInformation();
-//artist.DisplayMusicsInformation();
-//album1.DisplayInformation();
-
-//artist.AddRating(new Rating(5));
-//artist.AddRating(new Rating(5));
-//artist.AddRating(new Rating(4));
-
-
-//music1.AddRating(new Rating(5));
-//music1.AddRating(new Rating(4));
-//music1.AddRating(new Rating(4));
-
-
-//Console.WriteLine($"Average ratings for the artist {artist.Name}: {artist.GetAverageRating()}");
-//Console.WriteLine($"Average ratings for the song {music1.Name}: {music1.GetAverageRating()}");
-
 Dictionary<string, Artist> artists = [];
 MenuManager menuManager = new MenuManager();
 int menuOption = 0;
@@ -51,24 +22,31 @@ do
     {
         case 1:
             menuManager.RegisterArtist();
+            Console.Clear();
             break;
         case 2:
             menuManager.RegisterAlbum();
+            Console.Clear();
             break;
         case 3:
             menuManager.RegisterMusic();
+            Console.Clear();
             break;
         case 4:
             menuManager.ShowRegisteredArtists();
+            Console.Clear();
             break;
         case 5:
             menuManager.AddRatingToArtist();
+            Console.Clear();
             break;
         case 6:
             menuManager.AddRatingToMusic();
+            Console.Clear();
             break;
         case 7:
             menuManager.ViewArtistDetails();
+            Console.Clear();
             break;
         default:
             Console.WriteLine("Invalid option.");
