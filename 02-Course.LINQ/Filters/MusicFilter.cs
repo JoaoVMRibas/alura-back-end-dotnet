15c717081b0109177815efa71d25eeed923c7cdb
João Ribas <joaovitormribas@gmail.com>
2026-07-15 23:26:26 -0300

    Using LINQ to filter a list of songs

---
+++ 02-Course.LINQ/Filters/MusicFilter.cs
@@ -0,0 +1,43 @@
+﻿using Course.LINQ.Models;
+
+namespace Course.LINQ.Filters;
+
+internal class MusicFilter
+{
+    public static List<string?> FilterAllDistinctMusicalGenres(List<Music> musics)
+    {
+        return musics.Select(music => music.Genre)
+            .Distinct()
+            .ToList();
+    }
+
+    public static List<string?> FilterAllArtistNamesInAscendingOrder(List<Music> musics)
+    {
+        return musics.Select(music => music.Artist)
+            .Distinct()
+            .OrderBy(artist => artist)
+            .ToList();
+    }
+
+    public static List<string?> FilterAllArtistNamesInDescendingOrder(List<Music> musics)
+    {
+        return musics.Select(music => music.Artist)
+            .Distinct()
+            .OrderByDescending(artist => artist)
+            .ToList();
+    }
+
+    public static List<string?> FilterArtistsByMusicalGenre(List<Music> musics, string genre)
+    {
+        return musics.Where(music => music.Genre != null && music.Genre.Contains(genre))
+            .Select(music => music.Artist)
+            .Distinct()
+            .ToList();
+    }
+
+    public static List<Music> FilterMusicsByArtist(List<Music> musics, string artist)
+    {
+        return musics.Where(music => string.Equals(music.Artist, artist, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+    }
+}
--- 02-Course.LINQ/Program.cs
+++ 02-Course.LINQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using Course.LINQ.Models;
+﻿using Course.LINQ.Filters;
+using Course.LINQ.Models;
 using System.Text.Json;
 
 using (HttpClient client = new HttpClient())
@@ -6,12 +7,46 @@ using (HttpClient client = new HttpClient())
     try
     {
         string response = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        
+
         var songs = JsonSerializer.Deserialize<List<Music>>(response) ?? [];
 
-        songs[0].DisplayInformation();
+        // Displays all distinct musical genres in the music list
+        var genres = MusicFilter.FilterAllDistinctMusicalGenres(songs) ?? [];
+        foreach (var genre in genres)
+        {
+            //Console.WriteLine($"- {genre}");
+        }
+
+        // Displays all distinct artists in the music list in ascending order
+        var artistsAsc = MusicFilter.FilterAllArtistNamesInAscendingOrder(songs) ?? [];
+        foreach (var artist in artistsAsc)
+        {
+            //Console.WriteLine($"- {artist}");
+        }
+
+        // Displays all distinct artists in the music list in descending order
+        var artistsDes = MusicFilter.FilterAllArtistNamesInDescendingOrder(songs) ?? [];
+        foreach (var artist in artistsDes)
+        {
+            //Console.WriteLine($"- {artist}");
+        }
+
+        // Displays all artists by the selected musical genre
+        var artistsByGenre = MusicFilter.FilterArtistsByMusicalGenre(songs, "rock") ?? [];
+        foreach (var artist in artistsByGenre)
+        {
+            //Console.WriteLine($"- {artist}");
+        }
+
+        // Displays all songs by the artist
+        var songsByArtist = MusicFilter.FilterMusicsByArtist(songs, "Michael Jackson") ?? [];
+        foreach (var song in songsByArtist)
+        {
+            //song.DisplayInformation();
+            //Console.WriteLine();
+        }
     }
-    catch(Exception ex)
+    catch (Exception ex)
     {
         Console.WriteLine($"Error in GET request: {ex.Message}");
     }
