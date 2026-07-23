7b32d41eaed74995242e0863e36ad56d96cc4dbe
João Ribas <joaovitormribas@gmail.com>
2026-07-22 21:54:25 -0300

    Adding basic class logic

--- 03-Course.Archives/Models/CheckingAccount.cs
+++ 03-Course.Archives/Models/CheckingAccount.cs
@@ -2,4 +2,39 @@
 
 internal class CheckingAccount
 {
+    public int Number { get; }
+    public int Branch { get; }
+    public double Balance { get; private set; }
+    public Client Holder { get; set; }
+
+    public CheckingAccount(int branch, int number, Client holder)
+    {
+        Branch = branch;
+        Number = number;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Deposit amount must be greater than zero.", nameof(amount));
+        }
+
+        Balance += amount;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Withdrawal amount must be greater than zero.", nameof(amount));
+        }
+
+        if (amount > Balance)
+        {
+            throw new InvalidOperationException("Insufficient funds.");
+        }
+
+        Balance -= amount;
+    }
 }
--- 03-Course.Archives/Models/Client.cs
+++ 03-Course.Archives/Models/Client.cs
@@ -2,4 +2,7 @@
 
 internal class Client
 {
+    public string Name { get; set; } = string.Empty;
+    public string TaxId { get; set; } = string.Empty;
+    public string Occupation { get; set; } = string.Empty;
 }
