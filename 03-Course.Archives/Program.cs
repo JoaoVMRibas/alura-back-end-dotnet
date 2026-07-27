7580c658d278ad7adaafd6be95a45296ab62dd11
João Ribas <joaovitormribas@gmail.com>
2026-07-27 20:50:03 -0300

    Adding file reading with FileStream and StreamReader

--- 03-Course.Archives/Program.cs
+++ 03-Course.Archives/Program.cs
@@ -1,23 +1,6 @@
-﻿using System.Text;
+﻿using Course.Archives.Services;
 
 var path = "CheckingAccount.txt";
 
-using (var fs = new FileStream(path, FileMode.Open))
-{
-    var buffer = new byte[1024];
-    var qTBytes = -1;
-
-    while (qTBytes != 0)
-    {
-        qTBytes = fs.Read(buffer, 0, 1024);
-
-        WriteBuffer(buffer, qTBytes);
-    }
-}
-
-static void WriteBuffer(byte[] buffer, int qTBytes)
-{
-    var encoding = new UTF8Encoding();
-    var text = encoding.GetString(buffer, 0, qTBytes);
-    Console.Write(text);
-}
+var reader = new AccountFileReader(path);
+reader.ReadAndDisplayFile();
---
+++ 03-Course.Archives/Services/AccountFileReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Course.Archives.Services;
+
+internal class AccountFileReader
+{
+    private readonly string _filePath;
+
+    public AccountFileReader(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public void ReadAndDisplayFile()
+    {
+        if(!File.Exists(_filePath))
+        {
+            Console.WriteLine("File not found.");
+            return;
+        }
+
+        using (var fs = new FileStream(_filePath, FileMode.Open))
+        {
+            var reader = new StreamReader(fs);
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
